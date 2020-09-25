2abaccdfd0fe855e3d7538a76572d7675c9868bc
Benjamin Atkin <ben@resources.co>
2020-09-25 18:38:13 -0400

    make it work on safari; change order of declaration

--- app.js
+++ app.js
@@ -3,54 +3,28 @@ import htm from 'https://unpkg.com/htm?module';
 
 const html = htm.bind(h);
 
-function init() {
-  render(html`<${App}/>`, document.body);
+function SwitchBorderImage() {
+  return html`<svg width="100%" height="100%" viewBox="0 0 60 35">
+    <linearGradient id="linearColors1" x1="0" y1="0" x2="1" y2="1">
+      <stop offset="0%" stop-color="#999"></stop>
+      <stop offset="25%" stop-color="#ccc"></stop>
+      <stop offset="75%" stop-color="#ccc"></stop>
+      <stop offset="100%" stop-color="#999"></stop>
+    </linearGradient>
+    <path d="m 17.5 0 a 17.5 17.5 0 0 0 0 35 l 25 0 a 17.5 17.5 0 0 0 0 -35 l -25 0"
+        fill="none" stroke="url(#linearColors1)" stroke-width="10" />
+  </svg>`
 }
 
-class App extends Component {
-  componentDidMount() {
-    const mql = window.matchMedia("(min-width: 640px)");
-    this.setState({splitView: mql.matches});
-    mql.addEventListener('change', this.handleMatchMediaChange);
-  }
-
-  handleMatchMediaChange = ({matches}) => {
-    this.setState({splitView: matches});
-  }
-
-  render({}, {splitView}) {
-    return html`<${ViewportHeightFix}>
-      ${splitView ? html`<${SplitLayout}/>` : html`<${SwitchLayout}/>`}
-    </${ViewportHeightFix}>`
-  }
-}
-
-// https://github.com/Faisal-Manzer/postcss-viewport-height-correction
-class ViewportHeightFix extends Component {
-  state = { clientHeight: null };
-
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
-    this.handleResize()
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
-  }
-
-  handleResize = () => {
-    const newClientHeight = document.documentElement.clientHeight;
-    if (newClientHeight !== this.state.clientHeight) {
-      this.setState({clientHeight: newClientHeight});
-      requestAnimationFrame(() => {
-        document.documentElement.style.setProperty('--vh', (newClientHeight * 0.01) + 'px');
-      });
-    }
-  }
-
-  render({children}) {
-    return children;
-  }
+function Switch() {
+  return html`<div class="switch-box">
+    <${SwitchBorderImage}/>
+    <div class="switch-border">
+      <div class="switch">
+        <div class="knob"></div>
+      </div>
+    </div>
+  </div>`
 }
 
 function SwitchLayout() {
@@ -70,28 +44,64 @@ function SplitLayout() {
   </div>`
 }
 
-function Switch() {
-  return html`<div class="switch-box">
-    <${SwitchBorderImage}/>
-    <div class="switch-border">
-      <div class="switch">
-        <div class="knob"></div>
-      </div>
-    </div>
-  </div>`
+// https://github.com/Faisal-Manzer/postcss-viewport-height-correction
+class ViewportHeightFix extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { clientHeight: null };
+    this.handleResize = this.handleResize.bind(this.handleResize);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize() {
+    const newClientHeight = document.documentElement.clientHeight;
+    if (newClientHeight !== this.state.clientHeight) {
+      this.setState({clientHeight: newClientHeight});
+      requestAnimationFrame(() => {
+        document.documentElement.style.setProperty('--vh', (newClientHeight * 0.01) + 'px');
+      });
+    }
+  }
+
+  render({children}) {
+    return children;
+  }
 }
 
-function SwitchBorderImage() {
-  return html`<svg width="100%" height="100%" viewBox="0 0 60 35">
-    <linearGradient id="linearColors1" x1="0" y1="0" x2="1" y2="1">
-      <stop offset="0%" stop-color="#999"></stop>
-      <stop offset="25%" stop-color="#ccc"></stop>
-      <stop offset="75%" stop-color="#ccc"></stop>
-      <stop offset="100%" stop-color="#999"></stop>
-    </linearGradient>
-    <path d="m 17.5 0 a 17.5 17.5 0 0 0 0 35 l 25 0 a 17.5 17.5 0 0 0 0 -35 l -25 0"
-        fill="none" stroke="url(#linearColors1)" stroke-width="10" />
-  </svg>`
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleMatchMediaChange = this.handleMatchMediaChange.bind(this);
+  }
+
+  componentDidMount() {
+    const mql = window.matchMedia("(min-width: 640px)");
+    this.setState({splitView: mql.matches});
+    mql.addEventListener('change', this.handleMatchMediaChange);
+  }
+
+  handleMatchMediaChange({matches}) {
+    this.setState({splitView: matches});
+  }
+
+  render({}, {splitView}) {
+    return html`<${ViewportHeightFix}>
+      ${splitView ? html`<${SplitLayout}/>` : html`<${SwitchLayout}/>`}
+    </${ViewportHeightFix}>`
+  }
+}
+
+function init() {
+  console.log({App, document, html});
+  render(html`<${App}/>`, document.body);
 }
 
 init();
